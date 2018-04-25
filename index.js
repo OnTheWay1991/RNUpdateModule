5130b53a2ab92794fe511455f1ccca196e3b6c45
dongdongge <625324231@qq.com>
2018-04-25 09:40:50 +0800

    修改 入口文件

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent('RNUpdate0424', () => App);
+AppRegistry.registerComponent('RNUpdateModule', () => App);
